876363589715c9904cdc0d2fdb341a03d26db0a5
sbisi <62792590+sbisi@users.noreply.github.com>
2025-09-15 10:14:03 +0200

    Update user.py

--- backend/src/routes/user.py
+++ backend/src/routes/user.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
-from src.models.user import User, db
+from src.models.user import User
+from src.models.__init__ import db
 
 user_bp = Blueprint('user', __name__)
 
