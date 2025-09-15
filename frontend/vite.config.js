445c489e2eced72cd1c575c4deb08dbac570161f
sbisi <62792590+sbisi@users.noreply.github.com>
2025-09-15 13:51:27 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,6 @@ import path from 'node:path'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: { '@': path.resolve(__dirname, './src') },
   },
 })
